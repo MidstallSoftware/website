f4e2dda9eeb4011410b6202a179df4f783bda8aa
TheComputerGuy <spaceboyross@yandex.com>
2020-10-02 15:54:02 -0700

    Fixed typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
     meta: [
       { charset: 'UTF-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Midstall Software is an independent technology startup based in the Pacfic Northwest.' }
+      { hid: 'description', name: 'description', content: 'Midstall Software is an independent technology startup based in the Pacific Northwest.' }
     ]
   },
   buildModules: [
