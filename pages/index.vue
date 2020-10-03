ae98561c5a4f953a4325d59da5ffd6fb4123dffc
TheComputerGuy <spaceboyross@yandex.com>
2020-10-02 21:28:28 -0700

    Added link to Discord

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-col class="mx-auto" cols="6">
       <v-card>
         <v-card-title>Welcome</v-card-title>
-        <v-card-text>Welcome to the Midstall Software website.</v-card-text>
+        <v-card-text>Welcome to the Midstall Software website. We suggest joining our <a href="https://discord.gg/GVfBF2w">Discord server</a> to get updates.</v-card-text>
       </v-card>
     </v-col>
   </v-row>
