eb51c0088d1ddf443bfea65774766e05795f714f
RossComputerGuy <rosscomputerguy@protonmail.com>
2021-09-16 20:32:48 -0700

    Updated homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,22 @@
 <template>
-  <v-row dense>
-    <v-col class="mx-auto" cols="6">
-      <v-card>
-        <v-card-title>Welcome</v-card-title>
-        <v-card-text>Welcome to the Midstall Software website. We suggest joining our <a href="https://discord.gg/GVfBF2w">Discord server</a> to get updates.</v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+	<div>
+  	<v-row dense>
+	   	<v-col class="mx-auto" cols="6">
+	      <v-card>
+	        <v-card-title>Welcome</v-card-title>
+	        <v-card-text>We are Midstall Software, a group of passionate developers who hope to bring privacy and security to all devices. We are the people responsible for the Linux based operating system called <a href="https://expidusos.com">ExpidusOS</a>. We were founded in October of 2020 by <a href="https://twitter.com/RossComputerGuy">Tristan X. Ross</a> and <a href="https://twitter.com/drago_senpai">Travis Brinkert</a>.</v-card-text>
+	      </v-card>
+	    </v-col>
+	  </v-row>
+		<v-row dense>
+			<v-col class="mx-auto" cols="6">
+				<v-card>
+					<v-card-title>Interested in working for us?</v-card-title>
+					<v-card-text>We are always looking for passionate developers. Please send us a message on our <a href="https://discord.gg/GVfBF2w">Discord server</a>.</v-card-text>
+				</v-card>
+			</v-col>
+		</v-row>
+	</div>
 </template>
 <script>
 export default {
