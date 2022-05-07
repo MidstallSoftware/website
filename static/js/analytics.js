5e5e944ec52a0a0d9707716c8087cc0ab6afd01c
Tristan Ross <tristan.ross@midstall.com>
2022-05-06 22:04:49 -0700

    feat: analytics now work

---
+++ static/js/analytics.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+(function(i,s,o,g,r,a,m){i.GoogleAnalyticsObject=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'G-GGPX59G3D9', 'auto');
+setTimeout(function(){
+  window.ZAB.GAIntegFuncOld();
+  ga('send', 'pageview');
+},200);
